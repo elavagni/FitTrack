06c1653c480d3256aa144d290354540a9ebd8cbe
Eric Lavagni <LISSABE@capitol.local>
2019-12-15 20:38:23 -0600

    add logic for login component using using reactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { PastTrainingComponent } from './training/past-training/past-training.co
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -32,7 +32,8 @@ import { FormsModule } from '@angular/forms';
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  formGroup: FormGroup;
+  
   constructor() { }
 
+
   ngOnInit() {
+    this. createForm();
+  }
+
+  createForm() {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+      password: new FormControl('', {validators: [Validators.required]})});
+  }
+
+  onSubmit() {
+    console.log(this.formGroup);
   }
 
 }
