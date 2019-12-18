cc316d96f301afa3fcd585363ad304ea602d208f
LISSABE <LISSABE@L0059213>
2019-12-17 19:22:21 -0600

    provide dummy methods for login and logout, hide menu options depending on Auth state

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import {  Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
-
+export class SidenavListComponent implements OnInit, OnDestroy {
+ 
   @Output() closeSidenav  = new EventEmitter
+  isAuth: boolean = false;
+  private authChange: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authChange = this.authService.authChange.subscribe(isAuth => {
+        this.isAuth = isAuth;
+    })
   }
+
+  ngOnDestroy(): void {
+    this.authChange.unsubscribe();
+  }
+
 
   onClose() {
     this.closeSidenav.emit();
