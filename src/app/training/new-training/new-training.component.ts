a60f3fd4db7e558efca3419ea73beddd652684df
Eric Lavagni <LISSABE@capitol.local>
2019-12-22 18:18:50 -0600

    Add angularfire and firebase to project, get exercises from Firestore

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Output } from '@angular/core';
 import { TrainingService } from '../training/training.service';
 import { Excercise } from '../training/exercise.model';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'; 
 
 
 @Component({
@@ -10,13 +13,29 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Excercise[] = [];
+  exercises: Observable<any>;
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.exercises = this.trainingService.getAvaliableExercises();
-  } 
+    this.exercises =  this.db   
+        .collection('avaliableExercises')
+        .snapshotChanges()
+        .pipe(          
+            map(docArray => {
+              return docArray.map(doc => {
+                return {
+                  id:doc.payload.doc.id,
+                  name: doc.payload.doc.data()["name"],
+                  duration: doc.payload.doc.data()["duration"],
+                  calories: doc.payload.doc.data()["calories"]
+                }
+              });
+            })
+        )
+  }
+
+   
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
