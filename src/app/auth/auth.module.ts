d35085d458edf0623c06ddb4349d238b91e5a656
Eric Lavagni <LISSABE@capitol.local>
2019-12-23 22:25:22 -0600

    Added shared module, created routing module per feature

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,29 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MaterialModule } from '../material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
+import { SharedModule } from '../shared/shared.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
     declarations: [ 
         SignupComponent,
         LoginComponent
     ],
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MaterialModule,
-        FlexLayoutModule,
-        AngularFireAuthModule
-        
-    ],
-    exports:[
-
-    ]
+    imports: [       
+        ReactiveFormsModule,      
+        AngularFireAuthModule,
+        SharedModule,
+        AuthRoutingModule       
+    ]    
 })
 export class AuthModule { }
--- src/app/training/training/training.module.ts
+++ src/app/training/training/training.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { CurrentTrainingComponent } from '../current-training/current-training.component';
 import { PastTrainingComponent } from '../past-training/past-training.component';
 import { NewTrainingComponent } from '../new-training/new-training.component';
-import { MaterialModule } from 'src/app/material.module';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AuthModule } from 'src/app/auth/auth.module';
 import { TrainingComponent } from './training.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StopTrainingComponent } from '../current-training/stop-training-component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TrainingRoutingModule } from './training-routing.module';
 
 
 @NgModule({
@@ -23,19 +22,13 @@ import { StopTrainingComponent } from '../current-training/stop-training-compone
         StopTrainingComponent      
     ],
     imports: [
-        CommonModule,
-        MaterialModule,
-        AppRoutingModule,
-        FlexLayoutModule,            
-        AngularFirestoreModule,
-        FormsModule,
+       SharedModule,
+        AppRoutingModule,       
+        AngularFirestoreModule,       
         ReactiveFormsModule,
-        AuthModule,           
-             
-    ],
-    exports:[
-
-    ],
+        AuthModule,
+        TrainingRoutingModule         
+    ],    
     entryComponents: [StopTrainingComponent]
 })
 export class TrainingModule {
