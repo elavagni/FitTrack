81b9f728c7e65ded4f322bddea62671ef6c1b187
eric <elavagnim@gmail.com>
2020-09-06 01:02:45 -0500

    changes to upgrade to angular 10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
     { path: '', component: WelcomeComponent  }, 
-    { path: 'training', loadChildren: './training/training/training.module#TrainingModule', canLoad: [AuthGuard]  } 
+    { path: 'training', loadChildren: () => import('./training/training/training.module').then(m => m.TrainingModule), canLoad: [AuthGuard]  } 
 ];
 
 @NgModule({
